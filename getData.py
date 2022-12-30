f5e35648731a3dc1bc0d7f45d137bae2c43a6ce7
wmastover <wmastover@gmail.com>
2022-12-30 22:05:22 +0000

    uncommented

--- getData.py
+++ getData.py
@@ -8,22 +8,23 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from deleteCars import deleteCarsFunc 
 from uploadCars import uploadCarsFunc
-# data = pd.read_csv("carsToCollect.csv",  engine='python')
-# data_dict = data.to_dict(orient="records")
+
+data = pd.read_csv("carsToCollect.csv",  engine='python')
+data_dict = data.to_dict(orient="records")
 
 
 # # run web scraper for each car / year in carsToCollect.csv
-# for x in data_dict:
-#     make = x.get("Make")
-#     model = x.get("Model")
+for x in data_dict:
+    make = x.get("Make")
+    model = x.get("Model")
 
-#     print(make, model)
-#     x = 2005
+    print(make, model)
+    x = 2005
 
-#     #run get cars function for all years of each model
-#     while x < 2021:
-#         getCars(make, model, str(x))
-#         x = x + 1
+    #run get cars function for all years of each model
+    while x < 2021:
+        getCars(make, model, str(x))
+        x = x + 1
 
 date = datetime.today().strftime('%d%m%Y')
 directory = os.fsencode(date)
